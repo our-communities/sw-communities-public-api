b2725e9460bc38b9658d621878e405ba5f5e13d4
Tony Edwards <tony.edwards@gmail.com>
2019-03-17 21:29:54 +0000

    Adds local and production routes

--- express/server.js
+++ express/server.js
@@ -4,8 +4,8 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -15,27 +15,32 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 router.get('/api/v1', (req, res) => {
+  console.log('API v1 route hit');
 
-  //fetch('https://southwestcommunities.co.uk/api/v1/data.json')
-  //.then(res.json({ data : req.body }));
   fetch('https://southwestcommunities.co.uk/api/v1/data.json')
    .then(res => res.json())
    .then(data => {
-      res.write( data );
+     res.json({ data : data });
    })
    .catch(err => {
-      res.write(err);
+      console.log(err);
    });
-
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  // res.write('<h1>Hello from Express.js API!</h1>');
-  // res.end();
 });
 
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+if (process.env.CONTEXT){
+  app.use('/.netlify/functions/server', router); // path must route to lambda
+} else {
+  app.use('/', router);
+}
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+// This file loads!
+// fetch('https://southwestcommunities.co.uk/api/v1/data.json', {mode: 'no-cors'})
+//  .then((response) => response.json())
+//  .then((responseText) => {
+//      console.log(responseText);
+//  });
