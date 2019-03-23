d3ad93a6531ed5f62cb491b2b892627a74c7c438
Tony Edwards <tony.edwards@gmail.com>
2019-03-23 18:14:18 +0000

    Uncomment some of the things

--- express/server.js
+++ express/server.js
@@ -15,92 +15,92 @@ router.get('/', (req, res) => {
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 //
-// router.get('/api/v1', (req, res) => {
-//   console.log('API v1 route hit');
-//
-//   try {
-//   fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
-//     mode: 'no-cors'
-//   })
-//    .then(res => {
-//      let results = res.json();
-//      console.log(results);
-//      return results;
-//    })
-//    .then(data => {
-//      console.log(data);
-//      res.json({ data : data });
-//    })
-//    .catch(err => {
-//       console.log(err);
-//       res.writeHead(200, { 'Content-Type': 'text/html' });
-//       res.write(err);
-//       res.end();
-//    });
-//  }catch (err){
-//    console.log(err);
-//    res.writeHead(200, { 'Content-Type': 'text/html' });
-//    res.write(err);
-//    res.end();
-//  }
-// });
-//
-// router.get('/api/v1/eventByID/:id', (req, res) => {
-//   fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
-//     mode: 'no-cors'
-//   })
-//   .then(res => res.json())
-//   .then(data => {
-//     let thisEvent = Events.eventByID(data, req.params.id);
-//     res.json({ event : thisEvent });
-//   }).catch(err => {
-//     console.log(err);
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(err);
-//     res.end();
-//   });
-// });
-//
-// router.get('/api/v1/allEventsByOrganiser/:id', (req, res) => {
-//   fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
-//     mode: 'no-cors'
-//   })
-//   .then(res => res.json())
-//   .then(data => {
-//     let theseEvents = Events.allEventsByOrganiser(data, req.params.id);
-//     res.json({ event : theseEvents });
-//   }).catch(err => {
-//     console.log(err);
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(err);
-//     res.end();
-//   });
-// });
-//
-// router.get('/api/v1/nextEventByOrganiser/:id', (req, res) => {
-//   fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
-//     mode: 'no-cors'
-//   })
-//   .then(res => res.json())
-//   .then(data => {
-//     let theseEvents = Events.nextEventByOrganiser(data, req.params.id);
-//     res.json({ event : theseEvents });
-//   }).catch(err => {
-//     console.log(err);
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(err);
-//     res.end();
-//   });
-// });
+router.get('/api/v1', (req, res) => {
+  console.log('API v1 route hit');
+
+  try {
+  fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
+    mode: 'no-cors'
+  })
+   .then(res => {
+     let results = res.json();
+     console.log(results);
+     return results;
+   })
+   .then(data => {
+     console.log(data);
+     res.json({ data : data });
+   })
+   .catch(err => {
+      console.log(err);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(err);
+      res.end();
+   });
+ }catch (err){
+   console.log(err);
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.write(err);
+   res.end();
+ }
+});
+
+router.get('/api/v1/eventByID/:id', (req, res) => {
+  fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
+    mode: 'no-cors'
+  })
+  .then(res => res.json())
+  .then(data => {
+    let thisEvent = Events.eventByID(data, req.params.id);
+    res.json({ event : thisEvent });
+  }).catch(err => {
+    console.log(err);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(err);
+    res.end();
+  });
+});
+
+router.get('/api/v1/allEventsByOrganiser/:id', (req, res) => {
+  fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
+    mode: 'no-cors'
+  })
+  .then(res => res.json())
+  .then(data => {
+    let theseEvents = Events.allEventsByOrganiser(data, req.params.id);
+    res.json({ event : theseEvents });
+  }).catch(err => {
+    console.log(err);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(err);
+    res.end();
+  });
+});
+
+router.get('/api/v1/nextEventByOrganiser/:id', (req, res) => {
+  fetch('https://southwestcommunities.co.uk/api/v1/data.json', {
+    mode: 'no-cors'
+  })
+  .then(res => res.json())
+  .then(data => {
+    let theseEvents = Events.nextEventByOrganiser(data, req.params.id);
+    res.json({ event : theseEvents });
+  }).catch(err => {
+    console.log(err);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(err);
+    res.end();
+  });
+});
 
 app.use(bodyParser.json());
 
 // Make app work locally and remotely
-if (process.env.CONTEXT){
+// if (process.env.CONTEXT){
   app.use('/.netlify/functions/server', router); // path must route to lambda
-} else {
-  app.use('/', router);
-}
+// } else {
+  // app.use('/', router);
+// }
 
 
 module.exports = app;
