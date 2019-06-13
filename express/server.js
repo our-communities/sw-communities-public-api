fffa7c081e239c81ece8a74eb618a6da581435ec
Tony Edwards <tony.edwards@gmail.com>
2019-06-13 21:51:09 +0100

    Changes resonse to a json object
    prevents refactor elsewhere

--- express/server.js
+++ express/server.js
@@ -70,7 +70,7 @@ router.get('/api/v1/aroundDate/:date', (req, res) => {
   .then(res => res.json())
   .then(data => {
     let results = Events.aroundDate(data, req.params.date);
-    res.json(JSON.stringify(results));
+    res.json({ event : results });
   }).catch(err => {
     console.log(err);
     res.writeHead(200, { 'Content-Type': 'text/html' });
